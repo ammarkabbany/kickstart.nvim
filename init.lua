be0446c892cec0d685df0cf7c17b68ac77b71c8a
ammarkabbany <ammarss750@gmail.com>
2024-10-28 15:21:19 +0300

    some fixes

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -121,6 +121,12 @@ end)
 -- Enable break indent
 vim.opt.breakindent = true
 vim.opt.hidden = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.autoindent = true
 
 -- Save undo history
 vim.opt.undofile = true
@@ -149,7 +155,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
@@ -454,7 +460,7 @@ require('lazy').setup({
       }
       require('bufferline').setup {}
 
-      vim.keymap.set('n', '<leader>e', '<CMD>Neotree toggle position=right<CR>', { desc = 'Open neo-tree' })
+      vim.keymap.set('n', '<leader>e', '<CMD>Neotree toggle float<CR>', { desc = 'Open neo-tree' })
       -- vim.keymap.set('n', '<leader>b', '<CMD>Neotree toggle buffers<CR>', { desc = 'Open buffers tree' })
       vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>', { desc = 'Cycle to Next Buffer' })
       vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>', { desc = 'Cycle to Previous Buffer' })
